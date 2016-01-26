9847d0e63c7a514a6a9e4136cb3bc0e03765bd6b
Alex Ross <alex@enplug.com>
2016-01-25 18:53:45 -0800

    Fix ngAnnotate build bug

--- grunt/ngAnnotate.js
+++ grunt/ngAnnotate.js
@@ -4,7 +4,7 @@ module.exports = {
     },
     release: {
         files: {
-            'dist/<%= project %>/js/app.js': ['tmp/<%= project %>/app.js']
+            'dist/<%= project %>/js/app.js': ['tmp/<%= project %>/js/app.js']
         }
     }
 };
