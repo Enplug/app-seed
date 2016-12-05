7dac74e11ae03e634b39497e16844393c6bc2f38
Becky Beauchamp <beckybeauchamp1@gmail.com>
2016-12-05 15:47:38 -0800

    Update build script to use https for dashboard and http for player

--- scripts/build.js
+++ scripts/build.js
@@ -84,7 +84,7 @@ function runBuild () {
    }
    command += ' '+(needsDevServer?'webpack-dev-server':'webpack'); // webpack server or regular webpack?
    command += ' --config webpack.config.'+target+'.'+ (needsDevServer?'dev':'prod' ) +'.js '; // proper config, proper target
-   command += needsDevServer ? ' --watch --colors --inline  --progress  --host 0.0.0.0 ' + (target == pkg.config.targets[0] ? '--https' : '') : ' --bail --progress --profile ';
+   command += needsDevServer ? ' --watch --colors --inline  --progress  --host 0.0.0.0 ' + (target == 'dashboard' ? '--https' : '') : ' --bail --progress --profile ';
    command += needsVersionDirectory ? ' --directory-version' : '';
 
   sh.exec( command, function(argument) {
