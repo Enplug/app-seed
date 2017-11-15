7a7a90963a0bd99316cf4176d0f1310297cfc88e
Michal Drewniak <drewniak@gmail.com>
2017-11-15 15:26:04 +0100

    Build fixes

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ep-appseed',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
