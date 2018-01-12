b3d33ab3c8a8fad4cbf272390b4653eebaadf622
Michal Drewniak <drewniak@gmail.com>
2018-01-12 16:53:30 +0100

    Merge fixes

---
+++ app/src/app/enplug.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+ @Injectable()
+ export class EnplugService {
+   appStatus = enplug.appStatus;
+   assets = enplug.assets;
+   notifications = enplug.notifications;
+   playRecorder = enplug.playRecorder;
+   settings = enplug.settings;
+   on = enplug.on;
+   off = enplug.off;
+   once = enplug.once;
+ }
