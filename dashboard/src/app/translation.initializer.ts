e80a7576a22a9019becc7279ab6ba17564d5fd04
Arkadiusz Gochnio <agochnio+github@gmail.com>
2018-11-28 17:27:38 +0100

    Not trying to fetch the translations for English.

--- dashboard/src/app/translation.initializer.ts
+++ dashboard/src/app/translation.initializer.ts
@@ -6,15 +6,16 @@ import { EnplugService } from './services/enplug.service';
 export function translationInitializer(enplug: EnplugService, translate: TranslateService) {
   return () => new Promise<void>(resolve => {
     enplug.account.getUser().then(({ data: { locale } }) => {
-      translate.getTranslation(locale).pipe(catchError(() => EMPTY))
-        .subscribe(
-          translations => {
-            translate.setTranslation('locale', translations);
-            translate.use(locale);
-          },
-          null,
-          resolve
-        );
+      if (locale !== 'en') {
+        translate.getTranslation(locale).pipe(catchError(() => EMPTY))
+          .subscribe(
+            translations => {
+              translate.setTranslation('locale', translations);
+              translate.use(locale);
+            },
+            null,
+            resolve);
+      }
     });
   });
 }
