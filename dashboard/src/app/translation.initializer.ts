eaeab000f98ceda19a8660e6a57f08818b08781a
Bartek Polanczyk <bartosz.polanczyk@gmail.com>
2019-07-10 14:27:33 +0200

    Removed tedious code from initializer.

--- dashboard/src/app/translation.initializer.ts
+++ dashboard/src/app/translation.initializer.ts
@@ -4,42 +4,25 @@ import { catchError } from 'rxjs/operators';
 
 import { EnplugService } from './services/enplug.service';
 
-const GET_USER_TIMEOUT = 2000;
-
 export function translationInitializer(enplug: EnplugService, translate: TranslateService) {
   return () => new Promise<void>((resolve, reject) => {
-    let complete = false;
-
-    // Timeout to listen to getUser. If called from outside Enplug, will time out.
-    // Remove this for production code - it's here only for the tests
-    setTimeout(() => {
-      if (!complete) {
-        return resolve();
+    return enplug.account.getUser().then(({ data: { locale } }) => {
+      if (locale && typeof locale === 'string') {
+        translate.getTranslation(locale).pipe(catchError(() => EMPTY)).subscribe(
+          translations => {
+            translate.setTranslation('locale', translations);
+            translate.use(locale);
+          },
+          reject,
+          () => {
+            resolve();
+          }
+        );
+      } else {
+        resolve();
       }
-    }, GET_USER_TIMEOUT);
-
-    return enplug.account.getUser().then(
-      ({ data: { locale } }) => {
-        if (locale && typeof locale === 'string' && locale.substr(0, 2) !== 'en') {
-          translate.getTranslation(locale).pipe(catchError(() => EMPTY)).subscribe(
-            translations => {
-              translate.setTranslation('locale', translations);
-              translate.use(locale);
-            },
-            reject,
-            () => {
-              complete = true;
-              resolve();
-            }
-          );
-        } else {
-          complete = true;
-          resolve();
-        }
-      }).catch(
-      (err) => {
-        complete = true;
-        return reject(err);
-      });
+    }).catch((err) => {
+      return reject(err);
+    });
   });
 }
