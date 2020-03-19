49f168f41c451a66be369f6ee59fd3806a2eb229
Arkadiusz Gochnio <agochnio+github@gmail.com>
2020-03-19 09:55:26 +0100

    Updating to sdk-dashboard

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { HttpClientModule } from '@angular/common/http'
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,8 +8,6 @@ import { AssetListComponent } from './asset-list/asset-list.component';
 import { AssetComponent } from './asset/asset.component';
 import { AssetResolver } from './resolvers/asset.resolver';
 import { AssetsResolver } from './resolvers/assets.resolver';
-import { EnplugService } from './services/enplug.service';
-import { translationInitializer } from './translation.initializer';
 
 @NgModule({
   declarations: [
@@ -22,20 +19,10 @@ import { translationInitializer } from './translation.initializer';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    TranslateModule.forRoot({})
   ],
   providers: [
     AssetResolver,
     AssetsResolver,
-    {
-      provide: APP_INITIALIZER,
-      useFactory: translationInitializer,
-      deps: [
-        EnplugService,
-        TranslateService
-      ],
-      multi: true,
-    },
   ],
   bootstrap: [
     AppComponent
